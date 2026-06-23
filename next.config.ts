8afd5a15e6b143d007d86d0036c9b30ac801a1c8
gdp-uno <uno@gdesign-partners.co.jp>
2026-06-23 23:00:07 +0900

    fix: revert trailingSlash (was causing 404 on staging)

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
   images: { unoptimized: true },
   assetPrefix: isTauri ? "/" : undefined,
   basePath: isStaging ? "/edit" : undefined,
-  trailingSlash: isStaging ? true : undefined,
 };
 
 export default nextConfig;
